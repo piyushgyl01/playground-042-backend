d154c34a07812909135c586a016a82c73e306ec0
piyush.gyl <piyush2022ug@gmail.com>
2025-04-13 12:15:08 +0530

    feat: get API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const { connectToDB } = require("./db/db.connect");
 
+const Manga = require("./models/manga.model");
+
 connectToDB();
 app.use(express.json());
 
@@ -18,6 +20,44 @@ app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.json("Manga Database! Browse your favorite mangas");
+});
+
+// async function insertData(data) {
+//   try {
+//     const savedData = await Manga.insertMany(data);
+//     console.log(savedData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// insertData(mangaData)
+
+app.get("/mangas", async (req, res) => {
+  try {
+    const mangas = await Manga.find();
+    res.json(mangas);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+
+app.get("/mangas/:id", async (req, res) => {
+  try {
+    const manga = await Manga.findById(req.params.id);
+
+    if (!manga) {
+      return res.status(404).json({ message: "Manga not found" });
+    }
+
+    res.json(manga);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
 });
 
 app.listen(4000, () => {
